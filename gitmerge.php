44d7c172c41a01f7bec412db8064c2c557f359d9
Lex <lex@gouwestadmm.nl>
2013-03-21 14:11:41 +0100

    gitmerge

--- gitmerge.php
+++ gitmerge.php
@@ -4,7 +4,7 @@ echo exec('whoami');
 echo exec('/usr/local/bin/git status');
 
 
-$output = exec('ssh -T git@github.com 2>?1');
+$output = exec('ssh -T git@github.com');
 var_dump($output);
 
 $output2 = exec('/usr/local/bin/git fetch origin 2>&1');
