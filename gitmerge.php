9c681e4e219350a2c3550690be96a219066a4bcf
Lex <lex@gouwestadmm.nl>
2013-03-21 14:06:31 +0100

    gitmerge

--- gitmerge.php
+++ gitmerge.php
@@ -7,5 +7,5 @@ $output2 = exec('/usr/local/bin/git fetch origin 2>&1');
 var_dump($output2);
 
 
-$output = exec('ssh -T git@github.com');
+$output = exec('ssh -T git@github.com 2>?1');
 var_dump($output);
