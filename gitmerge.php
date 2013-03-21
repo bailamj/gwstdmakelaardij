daf450d441ee8172e8b7c62d61d02c8edd43c23f
Lex <lex@gouwestadmm.nl>
2013-03-21 14:05:19 +0100

    update gitmerge

--- gitmerge.php
+++ gitmerge.php
@@ -5,3 +5,7 @@ echo exec('/usr/local/bin/git status');
 
 $output2 = exec('/usr/local/bin/git fetch origin 2>&1');
 var_dump($output2);
+
+
+$output = exec('ssh -T git@github.com');
+var_dump($output);
