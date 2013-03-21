66d581641717c5940715f9a549ec84762b5766c5
Lex <lex@gouwestadmm.nl>
2013-03-21 14:01:23 +0100

    Update gitmerge

--- gitmerge.php
+++ gitmerge.php
@@ -1,12 +1,7 @@
 <?php
 
 echo exec('whoami');
-echo exec('git status');
-
-exec('git fetch origin && git merge origin master');
-
-$output = shell_exec('ls -lart');
-echo "<pre>$output</pre>";
+echo exec('/usr/local/bin/git status');
 
 $output2 = exec('/usr/local/bin/git fetch origin 2>&1');
 var_dump($output2);
